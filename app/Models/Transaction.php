d3eae820f34e920ac40392161b20c85e70b04a04
Rinat144 <rinatik14@gmail.com>
2023-03-11 20:44:11 +0300

    добавил поля для изменения в таблицу transaction

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -2,10 +2,24 @@
 
 namespace App\Models;
 
+use App\Services\Transaction\Enum\TypeTurnoverEnum;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'product_id',
+        'amount_before',
+        'amount_after',
+        'amount',
+        'type',
+    ];
+
+    protected $casts = [
+        'type' => TypeTurnoverEnum::class,
+    ];
 }
